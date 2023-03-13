26fbdce991ec9c25028f4b638bf4e727e1cc28f9
nguewouo mabkop manick <nguewouom@gmail.com>
2023-03-13 05:24:10 +0100

    test backend

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::with('user')->get();
+        return response()->json($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'user_id' => 'required|integer|exists:users,id',
+        ]);
+
+        $post = Post::create($request->all());
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::with('user')->find($id);
+        if ($post) {
+            return response()->json($post);
+        } else {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'content' => 'sometimes|string',
+            'user_id' => 'sometimes|integer|exists:users,id',
+        ]);
+
+        $post = Post::find($id);
+        if ($post) {
+            $post->update($request->all());
+            return response()->json($post);
+        } else {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        if ($post) {
+            $post->delete();
+            return response()->json(['message' => 'Post deleted']);
+        } else {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+    }
+}
